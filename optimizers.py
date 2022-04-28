6a4ce4896c26a43009dd15fdb99ad59b803ef30f
Kyung Sunggu <48959435+babbu3682@users.noreply.github.com>
2022-04-28 16:33:16 +0900

    update
    update

--- optimizers.py
+++ optimizers.py
@@ -6,7 +6,7 @@ def create_optim(name, model, args):
         optimizer    = torch.optim.Adam(params=model.parameters(), lr=args.lr, betas=(0.9, 0.999), eps=1e-08, weight_decay=5e-4, amsgrad=False)
 
     elif name == 'adamw':
-        optimizer    = torch.optim.Adam(params=model.parameters(), lr=args.lr, betas=(0.9, 0.999), eps=1e-08, weight_decay=5e-4, amsgrad=False)
+        optimizer    = torch.optim.AdamW(params=model.parameters(), lr=args.lr, betas=(0.9, 0.999), eps=1e-08, weight_decay=5e-4, amsgrad=False)
     
     else :
         raise KeyError("Wrong optim name `{}`".format(name))        
